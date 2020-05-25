31019cf9c960e4aa77b844f84b031c47ea80a196
Tin Ho Trong <hotinbk@gmail.com>
2020-05-25 15:06:34 +0700

    second commit

--- scripts.js
+++ scripts.js
@@ -59,3 +59,102 @@ function hideAll() {
     $("#dimensions-tab-content").addClass("is-hidden");
 }
 // kết thúc tabs
+
+// Open modal in Photo Gallery 
+document.querySelector('#open-modal').addEventListener('click', function (event) {
+    event.preventDefault();
+    var modal = document.querySelector('.modal');  // assuming you have only 1
+    var html = document.querySelector('html');
+    modal.classList.add('is-active');
+    html.classList.add('is-clipped');
+
+    modal.querySelector('.modal-background').addEventListener('click', function (e) {
+        e.preventDefault();
+        modal.classList.remove('is-active');
+        html.classList.remove('is-clipped');
+    });
+});
+
+
+// Swiper Hero
+
+var swiper = new Swiper(".swiper-container-0", {
+    autoplay: {
+        delay: 2000,
+        reverseDirection: true,
+        disableOnInteraction: false,
+
+    },
+    speed: 2000,
+    preventInteractionOnTransition: true,
+    effect: "fade",
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    loop: true,
+    themeColor: '#00d1b2',
+});
+
+// Swiper slider - client review
+
+var swiper = new Swiper(".swiper-container", {
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false
+    },
+    speed: 2500,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 10,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+});
+
+//Swiper Find more property
+
+var swiper = new Swiper(".swiper-container-2", {
+    autoplay: {
+        delay: 2500,
+        reverseDirection: true,
+        disableOnInteraction: false
+    },
+    speed: 500,
+    slidesPerView: 1,
+    spaceBetween: 10,
+    // init: false,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+});
